b6c58f49dac5eabc5792bfe85037df6a1dd37bb7
Daniel <danieljimenezmorales3@gmail.com>
2021-12-20 12:19:25 +0100

    ranking enservidor

---
+++ dishonorfactions/src/main/java/es/withoutbrakes/dishonorfactions/RankingRow.java
@@ -0,0 +1,24 @@
+package es.withoutbrakes.dishonorfactions;
+
+public class RankingRow {
+	private String username;
+	private int points;
+	
+	public RankingRow() {}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+}
